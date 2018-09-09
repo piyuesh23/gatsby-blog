d64ecbee04ffc086246625f6638791bbfd42aa57
Piyuesh Kumar <piyuesh23@qed42.com>
2018-09-09 09:49:56 +0530

    Initial commit for gatsby blog.

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Default Starter',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-drupal',
+      options: {
+        baseUrl: 'http://piyuesh.me/'
+      },
+    },
+  ],
+}
